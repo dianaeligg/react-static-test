a6d1c8e35cb735bc3541646cf5327fdb1b2745ee
Diana García <diana.eli.gg@gmail.com>
2019-07-20 14:05:05 -0500

    Updates

---
+++ precache-manifest.4bbd9b3a048c4c94450c78c3b8748c8b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0053979bc480ff0e5d41576fa485c21f",
+    "url": "/react-static-test/index.html"
+  },
+  {
+    "revision": "bf8ad097ef610b3c5846",
+    "url": "/react-static-test/static/css/main.0a79a625.chunk.css"
+  },
+  {
+    "revision": "5d03e205e0e029dac33f",
+    "url": "/react-static-test/static/js/2.578c9238.chunk.js"
+  },
+  {
+    "revision": "bf8ad097ef610b3c5846",
+    "url": "/react-static-test/static/js/main.b71a9c02.chunk.js"
+  },
+  {
+    "revision": "00b544836fff2ea481c5",
+    "url": "/react-static-test/static/js/runtime~main.862fbcec.js"
+  }
+]);
